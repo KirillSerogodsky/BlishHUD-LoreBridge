9ea448b876cce5cf0ad322f81524eaf7de4871ef
Kirill Serogodsky <serogodsky.kirill@gmail.com>
2025-01-21 02:23:18 +0300

    fix text wrapping with CJK

--- Utils/DrawUtils2.cs
+++ Utils/DrawUtils2.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Linq;
 using System.Text;
 using FontStashSharp;
@@ -9,31 +8,59 @@ public static class DrawUtil2
 {
     public static string WrapTextSegment(SpriteFontBase spriteFont, string text, float maxLineWidth)
     {
-        // var words = text.Contains(' ') ? text.Split(' ') : text.ToArray().Select(c => c.ToString());
-        var words = text.Split(' ');
+        var isCjk = text.Any(IsCjk);
         var sb = new StringBuilder();
-        var lineWidth = 0f;
-        var spaceWidth = spriteFont.MeasureString("  ").X - spriteFont.MeasureString(" ").X;
-        var aWidth = spriteFont.MeasureString("a").X;
-
-        foreach (var word in words)
+        
+        if (isCjk)
         {
-            var wordWidth = spriteFont.MeasureString("a" + word).X - aWidth;
-            wordWidth = Math.Max(wordWidth, spriteFont.MeasureString(word + "a").X - aWidth);
-            
-            if (lineWidth + wordWidth < maxLineWidth)
+            var lineWidth = 0f;
+
+            foreach (var character in text)
             {
-                sb.Append(word + " ");
-                lineWidth += wordWidth + spaceWidth;
+                var charWidth = spriteFont.MeasureString(character.ToString()).X;
+
+                if (lineWidth + charWidth > maxLineWidth)
+                {
+                    sb.Append("\n");
+                    lineWidth = 0f;
+                }
+
+                sb.Append(character);
+                lineWidth += charWidth;
             }
-            else
+        }
+        else
+        {
+            var words = text.Split(' ');
+            var lineWidth = 0f;
+            var spaceWidth = spriteFont.MeasureString(" ").X;
+
+            foreach (var word in words)
             {
-                sb.Append("\n" + word + " ");
-                lineWidth = wordWidth + spaceWidth;
+                var wordWidth = spriteFont.MeasureString(word).X;
+
+                if (lineWidth + wordWidth < maxLineWidth)
+                {
+                    sb.Append(word + " ");
+                    lineWidth += wordWidth + spaceWidth;
+                }
+                else
+                {
+                    sb.Append("\n" + word + " ");
+                    lineWidth = wordWidth + spaceWidth;
+                }
             }
         }
 
         return sb.ToString();
+    }
+
+    private static bool IsCjk(char c)
+    {
+        return (c >= 0x4E00 && c <= 0x9FFF) || // CJK Unified Ideographs
+               (c >= 0x3040 && c <= 0x309F) || // Hiragana
+               (c >= 0x30A0 && c <= 0x30FF) || // Katakana
+               (c >= 0xFF00 && c <= 0xFFEF); // Full-width characters
     }
 
     public static string WrapText(SpriteFontBase spriteFont, string text, float maxLineWidth)
